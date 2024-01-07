5c5e078c0b92720026377c4b116a98a2da2edc8d
vcosmin2701 <v.cosmin2717@gmail.com>
2024-01-07 17:54:13 +0200

    Book Entity Implementation

---
+++ LBBSystem/src/main/java/com/lbbsystem/lbbsystem/entities/Book.java
@@ -0,0 +1,53 @@
+package com.lbbsystem.lbbsystem.entities;
+import jakarta.persistence.*;
+
+@Entity
+public class Book {
+    private Long bookId;
+    private String title;
+    private String author;
+    private String category;
+    private int stock;
+    
+    @Id
+    @GeneratedValue
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
